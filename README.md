e191a47f11b97b7519da6ac3c5e636f02baff56d
Yusuke Hino <madeinyusuke@hotmail.co.jp>
2020-07-04 17:58:45 +0900

    Change packagenize

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 refined repository for https://github.com/previtus/AttentionPipeline
+
+
+```
+docker-compose up -d
+```
+
+```
+docker exec -it pipedet bash
+```
---
+++ setup.py
@@ -0,0 +1,16 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name='pipedet',
+    version='0.1.0',
+    license='',
+    description='pipeline detection',
+
+    author='a',
+    author_email='a',
+    url='a',
+
+    packages=find_packages(where='pipedet'),
+    package_dir={'': 'pipedet'},
+)
